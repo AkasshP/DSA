e45fd335ae9bb72d95852ef51a031dff49e1e730
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-04-02 15:55:44 -0400

    Time: 272 ms (5.06%) | Memory: 18 MB (54.88%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,17 +1,18 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom={}
-        magaz={}
-        ransomNote_List = list(ransomNote)
-        magazine_list  = list(magazine)
-        check = list(set(ransomNote))
-        for i in range(len(ransomNote_List)): 
+       
+        if len(ransomNote) <= len(magazine):
+            ransom={}
+            magaz={}
+            ransomNote_List = list(ransomNote)
+            magazine_list  = list(magazine)
+            check = list(set(ransomNote))
+            for i in range(len(ransomNote_List)): 
                 if ransom.get(ransomNote_List[i]) is None: 
                     ransom[ransomNote_List[i]] = 1
                 else:
                      ransom[ransomNote_List[i]] = ransom.get(ransomNote_List[i]) + 1
                      
-        if len(ransomNote) <= len(magazine):
             for i in range(len(magazine_list)):
                 for j in range(len(check)):
                     if check[j] == magazine_list[i]:

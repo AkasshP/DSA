38387df28421b8fda8318dbad9661eb6f86e01ae
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-04-02 16:07:20 -0400

    Time: 35 ms (9.68%) | Memory: 18.3 MB (13.59%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -7,17 +7,17 @@ class Solution:
             ransomNote_List = list(ransomNote)
             magazine_list  = list(magazine)
             ransomNote_set = list(set(ransomNote))
-            for i in range(len(ransomNote_List)): 
-                if ransom.get(ransomNote_List[i]) is None: 
-                    ransom[ransomNote_List[i]] = 1
+            for i in ransomNote: 
+                if ransom.get(i) is None: 
+                    ransom[i] = 1
                 else:
-                     ransom[ransomNote_List[i]] = ransom.get(ransomNote_List[i]) + 1
+                    ransom[i] = ransom.get(i) + 1
 
             for i in magazine:
                 if magaz.get(i) is None:
                     magaz[i] = 1
                 else:
-                    magaz[i] = magaz[i] + 1   
+                    magaz[i] = magaz.get(i) + 1   
                 
             for i in ransomNote_set:
                 if ransom.get(i) == magaz.get(i,0) or ransom.get(i) < magaz.get(i,0):

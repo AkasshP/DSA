1b87d4759acf48190b0fc97f21cb6afe6d90c315
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-04-02 15:10:37 -0400

    Time: 326 ms (5.02%) | Memory: 18.5 MB (13.60%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,54 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        print(len(ransomNote),'note')
+        ransom={}
+        magaz={}
+        ransomNote_List = list(ransomNote)
+        if len(ransomNote) <= len(magazine):
+            check = list(set(ransomNote))
+            for i in range(len(ransomNote_List)): 
+                if ransom.get(ransomNote_List[i]) is None: 
+                    ransom[ransomNote_List[i]] = 1
+                else:
+                     ransom[ransomNote_List[i]] = ransom.get(ransomNote_List[i]) + 1
+            print(ransom,'ransom dictionary')
+            l1  = list(magazine)
+            i = 0
+            flag = False
+            wanted = []
+            unwanted = 0
+            print(l1,'checking')
+            while i <= len(l1) - 1:
+                flag = False
+                for j in range(len(check)):
+                    if check[j] == l1[i]:
+                        wanted.append(l1[i])
+                        if magaz.get(l1[i]) is None:
+                            magaz[l1[i]] = 1
+                        else:
+                            magaz[l1[i]] = magaz[l1[i]] + 1
+                        flag = True
+                if flag == False:
+                    unwanted =  unwanted + 1
+                i = i + 1
+            
+            print(magaz,'magazine dictionary')
+            print(unwanted,'count')
+            print(wanted,'wanted list')
+            result  = len(magazine) - unwanted
+            print(result,'result checking')
+            # if len(ransomNote) == result:
+            #     return True
+            # else:
+            #     return False
+            for i in check:
+                if ransom.get(i) == magaz.get(i,0) or ransom.get(i) < magaz.get(i,0):
+                    pass
+                else:
+                    return False
+                    break
+            return True
+
+        else:
+            return False
+                

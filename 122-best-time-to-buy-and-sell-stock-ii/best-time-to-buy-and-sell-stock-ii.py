3f355e96302844917bc97e379da457fdc958dc88
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-04-28 01:18:34 -0400

    Time: 4 ms (18.60%) | Memory: 19 MB (28.67%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -3,7 +3,7 @@ class Solution:
         tp=[]
         j = 0
         i = 0
-        if len(prices) == 2:
+        if len(prices) == 2: #use case for two value
             if prices[1] > prices[0]:
                 return prices[1] - prices[0]
         if len(prices) >= 3:
@@ -13,7 +13,7 @@ class Solution:
                     k = j + 1
                     if len(prices) > j:
                         if ptr < prices[j]:
-                            print('ptr-fixed',ptr)
+                            print('ptr-fixed',ptr) # ptr fixed for the price level checking
                             initial_profit = prices[j] - ptr
                             if j and i and k == len(prices):
                                 if prices[j] > prices[i]:
@@ -24,23 +24,23 @@ class Solution:
                             while len(prices) > k:
                                     if initial_profit  < prices[k] - ptr:
                                         if updated_profit < prices[k] - ptr:
-                                            updated_profit = prices[k] - ptr
+                                            updated_profit = prices[k] - ptr #updating the profit value
                                         else:
                                             break
                                         k += 1
                                     else:
                                         if initial_profit > updated_profit:
                                             updated_profit = initial_profit
-                                            print(updated_profit,'initial profit')
+                                            print(updated_profit,'initial profit')# intial profit
                                             break
                                         else:
                                             break
                             tp.append(updated_profit)
-                            i = k - 1
+                            i = k - 1 #skipping the iteration value
                         else:
                             pass
                         i += 1
-                        print(i,'updating the i value')
+                        #iterating the value
                     else:
                         break
         result = sum(tp) if tp else 0 

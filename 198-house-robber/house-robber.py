c1f7f0f461323da5054e30d6a989b83446616422
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-05-08 20:00:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.42%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,28 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
-            return max(nums)
         
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        
-        for i in range(2,len(nums)):
-            dp[i] = max(dp[i - 1],dp[i - 2] + nums[i])
-        return dp[-1]
+        #approach of like finding max value
+        pocket_A = 0 #skipping
+        pocket_B = 0 # taking
+        rob = []
+        for i in range(len(nums)):
 
+            result = max(pocket_A,nums[i] + pocket_B)
+            pocket_B = pocket_A
+            pocket_A = result 
+            rob.append(result)
 
-        
-
-
-
-
-
-
-
-
-         
+        return rob[-1]

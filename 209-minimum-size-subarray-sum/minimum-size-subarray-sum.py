3ca273eb25cc8b3e8e1eb33e0adf8199408f145a
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-05-09 23:23:49 -0400

    Time: 11 ms (47.59%) | Memory: 28.5 MB (10.99%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -2,18 +2,17 @@ import math
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left = 0
-        right = left + 1
+        right = 0
         min_count = math.inf
-        curr = sum(nums[left:right])
-        while right <= len(nums):
-            if curr >= target:
-                min_count = min(min_count,len(nums[left:right]))
+        curr = 0
+        while right < len(nums):
+            curr += nums[right]
+            while curr >= target:
+                min_count = min(min_count,right - left + 1) #i'm instead of slicing the elements 
                 curr -= nums[left]
                 left = left + 1
-            else:
-                if right < len(nums):
-                    curr += nums[right]
-                right = right + 1
+            right = right + 1
+
         if min_count == inf:
             return 0
         else:

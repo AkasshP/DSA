db58aa452d40b84730e1272b36b6599e5672ebdb
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-05-13 01:12:22 -0400

    Time: 740 ms (17.55%) | Memory: 149 MB (5.20%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -2,13 +2,11 @@ class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         #adjacency list
         nodes = {i: [] for i in range(n)}
-        print(nodes)
         for u, v in edges:
             nodes[u].append(v)
             nodes[v].append(u)
         seen = set()
         def dfs(src,dest):
-            print('src',src)
             if src == dest:
                 return True
             val = nodes[src]

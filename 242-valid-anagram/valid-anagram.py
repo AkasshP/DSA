52660839a5ac007dd71e3c4b27b7dd1c8c5c0594
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-06-15 23:24:11 -0400

    Time: 7 ms (89.79%) | Memory: 17.8 MB (63.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,28 +1,9 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            s_map = {}
-            t_map = {}
-            for i in s:
-                if s_map.get(i):
-                    s_map[i] = s_map[i] + 1
-                else:
-                    s_map[i] = 1
-            print(s_map,'s_map')
-            for i in t:
-                if t_map.get(i):
-                    t_map[i] = t_map[i] + 1
-                else:
-                    t_map[i] = 1
-            print(t_map,'s_map')
-            for key in s_map.keys():
-                if t_map.get(key):
-                    if s_map[key] == t_map[key]:
-                        pass
-                    else:
-                        return False
-                else:
-                    return False
+        freq1 = Counter(s)
+        freq2 = Counter(t)
+        if freq1 == freq2:
             return True
         else:
             return False

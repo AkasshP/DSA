fc5726d18bc3bb6e2790f60424f37be0b2a56781
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-06-22 16:27:44 -0400

    Time: 251 ms (5.03%) | Memory: 57.9 MB (5.82%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,36 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+
+
+        #step 1 valid triplets:
+        new_triplets = [[i[j] for j in range(len(i)) if i[j] <= target[j]] for i in triplets]
+        final_triplets = list(filter(lambda x: len(x) == 3,new_triplets))
+        if final_triplets:
+            #Step 2 filter based on the indexes basis
+            set_A = list(filter(lambda x: x <= target[0],list(map(lambda x: x[0],final_triplets))))
+            set_B = list(filter(lambda x: x <= target[1],list(map(lambda x:x[1],final_triplets))))
+            set_C = list(filter(lambda x: x <= target[2],list(map(lambda x:x[2],final_triplets))))
+            print(set_A)
+            print(set_B)
+            print(set_C)
+            #step 3 match the target
+            for i in range(len(target)):
+                if i == 0 :
+                    if target[i] in set_A:
+                        pass
+                    else:
+                        return False
+                if i == 1:
+                    if target[i] in set_B:
+                        pass
+                    else:
+                        return False
+                if i == 2:
+                    if target[i] in set_C:
+                        pass
+                    else:
+                        return False
+            return True
+        else:
+            return False
+

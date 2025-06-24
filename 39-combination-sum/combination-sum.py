ab2fe10e96f2eb0225e85952651122a4c4882b46
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-06-24 19:58:10 -0400

    Time: 587 ms (5.01%) | Memory: 18.2 MB (19.22%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        visited = set()
+        new_arr = []
+        def combinations(temp):
+            if sum(temp) == target:
+                arr = sorted(temp)
+                if tuple(arr) not in visited:
+                    visited.add(tuple(arr))
+                    new_arr.append(arr)
+                    return 
+
+            for i in candidates:
+                if i + sum(temp) <= target:
+                    temp.append(i)
+                    combinations(temp)
+                    if temp:
+                        temp.pop()
+        
+        
+        for i in candidates:
+            temp = []
+            temp.append(i)
+            combinations(temp)
+
+        return new_arr
+        
+        
+        

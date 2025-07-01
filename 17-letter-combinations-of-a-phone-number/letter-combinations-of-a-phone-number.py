c8f2b1e3484871bf69ce69b95e6708b04ae39486
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-07-01 12:40:13 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (22.93%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,31 +1,45 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # Phone keypad mapping
-        phone_map = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
-        if not digits:
-            return []
-        result = []
-        def backtrack(index,strr):
+        arr_str = {1:[],
+        2:['a','b','c'],
+        3:['d','e','f'],
+        4:['g','h','i'],
+        5:['j','k','l'],
+        6:['m','n','o'],
+        7:['p','q','r','s'],
+        8:['t','u','v'],
+        9:['w','x','y','z']}
 
-            if index == len(digits):
-                result.append(strr)
-                return
-            
-            letters = phone_map[digits[index]]
+        main_array = []
+        final_result = []
+        for i in digits:
+            main_array.append(arr_str[int(i)])
 
-            for l in letters:
-                backtrack(index + 1,strr + l)
-            
-        backtrack(0,"")
-        return result
-        
-        
+        def combinations(temp,i):
+            if len(temp) == len(digits):
+                final_result.append(''.join(temp))
+                return 
+
+            if i < len(main_array):
+                for j in main_array[i]:
+                    temp.append(j)
+
+                    if i < len(main_array):
+                        i += 1
+                    combinations(temp,i)
+                    temp.pop()
+                    i -= 1
+
+        if main_array:
+            if  len(main_array) > 1:
+                c = 1
+                for i in main_array[0]:
+                    temp = []
+                    temp.append(i)
+                    combinations(temp,c)
+            else:
+                return main_array[0]
+        else:
+            return main_array
+
+        return final_result

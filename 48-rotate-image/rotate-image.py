8e6c0f7a4b472617bb37adaa55f2185dfa2f050e
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-07-28 14:38:30 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (22.38%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,16 +4,37 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         r,c = len(matrix),len(matrix[0])
-        main = [[0] * c for _ in range(r)]
-        k = c - 1
+        # main = [[0] * c for _ in range(r)]
+        # k = c - 1
+        check = {}
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                main[i][k-j] = matrix[j][i]
+                check[str(i)+str(j)] = False
 
-        for i in range(len(main)):
-            for j in range(len(main[i])):
-                matrix[i][j] = main[i][j]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i == j:
+                    continue
+                elif check[str(i)+str(j)] == False:
+                    matrix[i][j],matrix[j][i] =  matrix[j][i],matrix[i][j]
+                    check[str(i)+str(j)] = True
+                    check[str(j)+str(i)] = True
+                else:
+                    continue
+
+        
+        
+        for i in range(len(matrix)):
+            k = c - 1
+            for j in range(len(matrix[i])): #two pointer techinique j is forward and k is backward once meets break the loop
+                if k <= j:
+                    break
+                else:
+                    matrix[i][j],matrix[i][k] = matrix[i][k],matrix[i][j]
+                k -= 1
                 
+        
+
 
         
         

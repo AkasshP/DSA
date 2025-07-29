66ad7fd85b108f9361ee5d7a6b73c6f1222fe2db
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-07-29 14:57:55 -0400

    Time: 5 ms (8.32%) | Memory: 18 MB (33.90%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,38 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        main_path = []
+        temp = []
+        dot = []
+        for i in path:
+            if i == '/':
+                if temp and ''.join(temp) != '.' and ''.join(temp) != '..':
+                    main_path.append(''.join(temp)) #append as single string
+                    temp = [] #reset it
+                else:
+                    if not temp: 
+                        continue 
+                    elif ''.join(temp) == '.':
+                        temp = [] #reset after '.', and don't add in stack
+                    else:
+                        if main_path: #value must be present
+                            main_path.pop() #since encountered '..'
+                        temp = [] #reset because the '..'
+
+            elif i == '.':
+                temp.append(i)
+            else:
+                temp.append(i)
+        if temp:
+            if ''.join(temp) == '..':
+                if main_path: #value must be present
+                    main_path.pop()
+            elif ''.join(temp) == '.':
+                pass
+            else:
+                main_path.append(''.join(temp))
+
+        
+        result = '/'.join(main_path)
+        return '/' + result
+
+            

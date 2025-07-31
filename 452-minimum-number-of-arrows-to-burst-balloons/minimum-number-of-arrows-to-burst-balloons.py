408815d8835ef4a86163cf616591b8a49986eda9
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-07-30 23:21:48 -0400

    Time: 117 ms (42.46%) | Memory: 52.1 MB (7.37%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -4,12 +4,11 @@ class Solution:
         st,end = final_points[0]
         shoot = 0
         main_check = {}
-        print(final_points,'final_points')
         for i in range(len(final_points)):
             main_check[i] = False
         for i in range(1,len(final_points)):
             x,y =  final_points[i]
-
+            
             if x <= end <= y:
                 if main_check[i-1] == False:
                     shoot += 1
@@ -21,12 +20,12 @@ class Solution:
         if all(i == True for i in main_check.values()):
             return shoot 
         else:
-            if all(i == False for i in main_check.values()): #since everything needs a sperate arrow
+            if all(i == False for i in main_check.values()): #since everything needs a seperate arrow
                 return len(final_points) 
             else:
                 cnt  = 0
                 for i in main_check.values():
                     if i == False:
                         cnt += 1
-
+        
                 return shoot + cnt

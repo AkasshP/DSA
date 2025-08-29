693f7f308f78be639c3037d16b7bb95637a33bfa
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-08-29 17:35:53 -0400

    Time: 55 ms (17.93%) | Memory: 45.7 MB (20.10%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,27 +1,21 @@
 import java.util.*;
 class Solution {
     public int minDeletions(String s) {
-        char[] ch = s.toCharArray();
         Map<Character,Integer> map = new HashMap<>();
         HashSet<Integer> main_set = new HashSet<>();
-        ArrayList<Integer> repetion_list = new ArrayList<>();
-        for(char x: ch) map.put(x,map.getOrDefault(x,0) + 1);
-        int min_del = 0;
-        for(int x: map.values()) 
+        for(char x: s.toCharArray()) map.put(x,map.getOrDefault(x,0) + 1);
+        int del = 0;
+        for(int i: map.values()) 
         {
-            if (!main_set.contains(x)) main_set.add(x);
-            else repetion_list.add(x);
-        }
-        for(int x:repetion_list)
-        {
-            int temp = x; // store the main copy
-            while (main_set.contains(x) && x != 0)
+            int x = i;
+            while(x > 0 && main_set.contains(x))
             {
-                x--; //decrement until new value found in set
+                x--;
+                del++;
             }
-            min_del += (temp - x != 0 ? temp - x : temp); 
-            if (x != 0) main_set.add(x);//update the minimum val in min_del
+            if (x > 0) main_set.add(x);
         }
-    return min_del;
+        
+    return del;
     }
 }

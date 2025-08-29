31633dfff0eeeb02ebf726cec3fb75c46405e526
AkasshP <68960332+AkasshP@users.noreply.github.com>
2025-08-29 17:18:11 -0400

    Time: 54 ms (20.87%) | Memory: 45.8 MB (20.10%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -7,14 +7,11 @@ class Solution {
         ArrayList<Integer> repetion_list = new ArrayList<>();
         for(char x: ch) map.put(x,map.getOrDefault(x,0) + 1);
         int min_del = 0;
-        System.out.println(map+"map");
         for(int x: map.values()) 
         {
             if (!main_set.contains(x)) main_set.add(x);
             else repetion_list.add(x);
         }
-        // System.out.println(repetion_list+"repetion");
-        // System.out.println(main_set+"set");
         for(int x:repetion_list)
         {
             int temp = x; // store the main copy
@@ -22,10 +19,8 @@ class Solution {
             {
                 x--; //decrement until new value found in set
             }
-            
             min_del += (temp - x != 0 ? temp - x : temp); 
             if (x != 0) main_set.add(x);//update the minimum val in min_del
-            
         }
     return min_del;
     }
